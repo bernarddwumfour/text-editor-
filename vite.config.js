68c493d6214ade4eb47d1135ae40b24ae3ed4149
khuccy <bernarddwumfour25@gmail.com>
2024-10-03 04:47:15 +0000

    Added other home pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: ['./index.html', './sheet.html', './views/docs.html','./notification.html'],
+      input: ['./index.html', './sheet.html', './views/docs.html','./notification.html','new.html','video-chat.html','test.html','shared.html','recycle-bin.html','recent.html','paper.html','favourite.html','chat.html','document.html'],
     },
   },
 });
